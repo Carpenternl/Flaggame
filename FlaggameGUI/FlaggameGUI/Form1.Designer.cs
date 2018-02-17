493339a5d5447bef2900a243e3681f84e3e48360
Carpenternl <Hugo.t.95@hotmail.com>
2018-02-17 14:31:21 +0100

    Simple Scoreboard added that activates on boolean condition

---
+++ FlaggameGUI/FlaggameGUI/Form1.Designer.cs
@@ -0,0 +1,60 @@
+﻿namespace FlaggameGUI
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.scoreBoard1 = new FlaggameGUI.ScoreBoard();
+            this.SuspendLayout();
+            // 
+            // scoreBoard1
+            // 
+            this.scoreBoard1.AutoSize = true;
+            this.scoreBoard1.Location = new System.Drawing.Point(404, 12);
+            this.scoreBoard1.Name = "scoreBoard1";
+            this.scoreBoard1.Size = new System.Drawing.Size(348, 179);
+            this.scoreBoard1.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(764, 353);
+            this.Controls.Add(this.scoreBoard1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private ScoreBoard scoreBoard1;
+    }
+}
+
---
+++ FlaggameGUI/FlaggameGUI/ScoreBoard.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FlaggameGUI
+{
+    public partial class ScoreBoard : UserControl
+    {
+        int currentscore = 0;
+        int lastscore;
+        int highscore;
+        double avg;
+        public ScoreBoard()
+        {
+            InitializeComponent();
+            recentscores = new List<bool>();
+            
+        }
+        public void updateScoreBoard(bool win)
+        {
+            if (win)
+            {
+                WinCondition();
+            }
+            else { LossCondition(); }
+            updateAverage(win);
+            updateDisplay();
+        }
+        private void WinCondition()
+        {
+            currentscore++;
+            if (highscore < currentscore)
+            {
+                highscore = currentscore;
+            }
+            
+        }
+        private void LossCondition()
+        {
+            lastscore = currentscore;
+            currentscore = 0;
+        }
+        private void updateDisplay()
+        {
+            LabelCurrenctScoreDisplay.Text = currentscore.ToString();
+            LabelLastScoreDisplay.Text = lastscore.ToString();
+            LabelHighScoreDisplay.Text = highscore.ToString();
+            LabelAccuracyDisplay.Text = "%"+((int)(avg * 100)).ToString();
+        }
+        /// <summary>
+        ///  returns the average accuracy of your last 100 answers
+        /// </summary>
+        /// <param name="wincondition">whether the current answer was correct or not</param>
+
+        List<bool> recentscores;
+        int loop = 0;
+        private void updateAverage(bool wincondition)
+        {
+            
+            // only create an average for the last 100 results
+            if (recentscores.Count < 100)
+            {
+                recentscores.Add(wincondition);
+            }
+            else
+            {  
+                recentscores[loop] = wincondition;
+                loop++;
+                if (loop >= recentscores.Count())
+                {
+                    loop = 0;
+                }
+
+            }
+            int wins = recentscores.Count(item => item == true);
+            int total = recentscores.Count();
+            avg = (double) wins / total;
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            updateScoreBoard(true);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            updateScoreBoard(false);
+        }
+    }
+}
