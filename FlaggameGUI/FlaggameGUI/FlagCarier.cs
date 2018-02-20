59cb72d79a14dd31015c27e4397e4e6ae6499ea6
Carpenternl <Hugo.t.95@hotmail.com>
2018-02-20 13:41:24 +0100

    Revert "Flagcarier will fetch flags from the image."
    This reverts commit ee04f192e6e9f59955cc362c06a55523a8ca446c.
    
    # Conflicts:
    #	FlaggameGUI/FlaggameGUI/FlagCarier.cs

--- FlaggameGUI/FlaggameGUI/FlagCarier.cs
+++ FlaggameGUI/FlaggameGUI/FlagCarier.cs
@@ -9,17 +9,8 @@ using System.Resources;
 namespace FlaggameGUI
 {
     
-    public class tile
-    {
-
-    }
     static class FlagCarier
     {
-
-        public static Image GetTile(int index)
-        {
-            return new Image;
-        }
 
     }
 }
