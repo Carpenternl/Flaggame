1c5a5f12a713028c21705db260c281b06e2cad06
Carpenternl <Hugo.Timmerman@outlook.com>
2018-03-12 15:33:44 +0100

    flag game working

--- FlaggameGUI/FlaggameGUI/Form1.Designer.cs
+++ FlaggameGUI/FlaggameGUI/Form1.Designer.cs
@@ -28,45 +28,33 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.gameViewControl1 = new FlaggameGUI.GameViewControl();
-            this.scoreBoard1 = new FlaggameGUI.ScoreBoardControl();
+            this.gameView1 = new FlaggameGUI.GameView();
             this.SuspendLayout();
             // 
-            // gameViewControl1
+            // gameView1
             // 
-            this.gameViewControl1.Location = new System.Drawing.Point(12, 12);
-            this.gameViewControl1.Name = "gameViewControl1";
-            this.gameViewControl1.Size = new System.Drawing.Size(412, 375);
-            this.gameViewControl1.TabIndex = 1;
-            // 
-            // scoreBoard1
-            // 
-            this.scoreBoard1.AutoSize = true;
-            this.scoreBoard1.Location = new System.Drawing.Point(430, 12);
-            this.scoreBoard1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
-            this.scoreBoard1.Name = "scoreBoard1";
-            this.scoreBoard1.Size = new System.Drawing.Size(250, 88);
-            this.scoreBoard1.TabIndex = 0;
+            this.gameView1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.gameView1.Location = new System.Drawing.Point(0, 0);
+            this.gameView1.Name = "gameView1";
+            this.gameView1.Size = new System.Drawing.Size(958, 588);
+            this.gameView1.TabIndex = 0;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(958, 588);
-            this.Controls.Add(this.gameViewControl1);
-            this.Controls.Add(this.scoreBoard1);
+            this.Controls.Add(this.gameView1);
             this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
         #endregion
 
-        private ScoreBoardControl scoreBoard1;
-        private GameViewControl gameViewControl1;
+        private GameView gameView1;
     }
 }
 
